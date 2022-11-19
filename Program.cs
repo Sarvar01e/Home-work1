89441c7929525a75c839133b2c2c8f85e5446312
user <user@WIN-AKM2LNADFM7>
2022-11-19 20:07:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Home_work1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number 1");
+
+            string a;
+
+             a = Console.ReadLine();
+
+            Console.WriteLine("Enter number 2");
+
+            string b;
+
+            b = Console.ReadLine();
+
+            int c = Convert.ToInt32(a);
+            int d =  Convert.ToInt32(b);
+
+            int result = (c + d) / 2;
+
+            Console.WriteLine("arithmetic mean is\t" + result);
+
+            
+        }
+    }
+}
